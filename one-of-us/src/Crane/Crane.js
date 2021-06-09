4d2446790f522131efff18e4d1ba49a24f2c9402
Nestor <nestor.venegas95@gmail.com>
2021-06-08 21:00:10 -0500

    crane to do

---
+++ one-of-us/src/Crane/Crane.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Crane = () => {
+
+    // TODO : Container 
+    // * Create container to encapsulate crane and objects.
+    // TODO : Crane 
+    // * Create crane.
+    // TODO : Controls 
+    // * Controls move crane left and right.
+    // * Button commands crane to "drop".
+    // TODO : Animation 
+    // * Update screen on frame refresh.
+    // * Position updates on refresh.
+    // TODO : Objects
+    // * Create objects for crane to grab.
+    // TODO : Collision Detection 
+    // * Collision detection between crane and container.
+    // * Collision detection between object and Crane.
+    // TODO : Pick Up Functionality
+    // * Object stops crane's full descent. 
+    // * Picked up success depends on where/how crane lands.
+
+    
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default Crane
+
